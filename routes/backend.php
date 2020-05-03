f1f15ec142b9535fec086630a72244e2ee6840fb
Usitha Rupasinghe <usitha@arimaclanka.com>
2020-05-03 21:59:36 +0530

    routes change

--- routes/backend.php
+++ routes/backend.php
@@ -27,46 +27,5 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/home', 'Backend\HomeController@home')->name('backend.home');
 
-    //traditional songs
-    Route::resource('traditional_songs', 'Backend\TraditionalSongController', ['as' => 'backend']);
-    //Knowledge Hub Videos
-    Route::resource('knowledge_hub_videos', 'Backend\KnowledgeHubVideosController', ['as' => 'backend']);
-    //Knowledge Hub Images
-    Route::resource('knowledge_hub_images', 'Backend\KnowledgeHubImagesController', ['as' => 'backend']);
-    //Knowledge Hub Audios
-    Route::resource('knowledge_hub_audios', 'Backend\KnowledgeHubAudiosController', ['as' => 'backend']);
-    //FAQ
-    Route::resource('faq', 'Backend\FrequentlyAskedQuestionsController', ['as' => 'backend']);
-    //song categories
-    Route::resource('song_categories', 'Backend\SongCategoryController', ['as' => 'backend']);
-    //hand work categories
-    Route::resource('hand_work_categories', 'Backend\HandWorkCategoryController', ['as' => 'backend']);
-    //hand work
-    Route::resource('hand_works', 'Backend\HandWorkController', ['as' => 'backend']);
-    //hand work images
-    Route::resource('hand_work_images', 'Backend\HandWorkImageController', ['as' => 'backend']);
-    //proverbs
-    Route::resource('proverbs', 'Backend\ProverbController', ['as' => 'backend']);
-    //traditional games
-    Route::resource('traditional_games', 'Backend\TraditionalGameController', ['as' => 'backend']);
-    //riddles
-    Route::resource('riddles', 'Backend\RiddleController', ['as' => 'backend']);
-    Route::resource('push_notifications', 'Backend\PushNotificationController', ['as' => 'backend']);
-
-    //new creations
-    Route::resource('new_creations', 'Backend\NewCreationController', ['as' => 'backend']);
-
-	Route::resource('user', 'Backend\UserController', ['except' => ['show'], 'as' => 'backend']);
-	Route::get('profile', ['as' => 'backend.profile.edit', 'uses' => 'Backend\ProfileController@edit']);
-	Route::put('profile', ['as' => 'backend.profile.update', 'uses' => 'Backend\ProfileController@update']);
-	Route::put('profile/password', ['as' => 'backend.profile.password', 'uses' => 'Backend\ProfileController@password']);
-
-    // Settings Routes
-    Route::get('settings/index',  ['as' => 'backend.settings.index', 'uses' => 'Backend\SettingController@index']);
-    Route::post('settings/update', ['as' => 'backend.settings.update', 'uses' => 'Backend\SettingController@update']);
-
-	Route::get('{page}', ['as' => 'backend.page.index', 'uses' => 'Backend\PageController@index']);
-
-
 });
 
