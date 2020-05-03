e2e7bccec21fd39d3bb68eef0e39cfa62301b5a9
Usitha Rupasinghe <usitha@arimaclanka.com>
2020-05-04 01:34:03 +0530

    Traditional Songs request files fixed

---
+++ app/Http/Requests/CMS/TraditionalSongUpdateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\CMS;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TraditionalSongUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title_en' => 'required',
+            'title_si' => 'required',
+            'title_ta' => 'required',
+            'writer_en' => 'required',
+            'writer_si' => 'required',
+            'writer_ta' => 'required',
+            'video_thumbnail' => 'nullable|image',
+            'video' => 'required|url',
+        ];
+    }
+}
