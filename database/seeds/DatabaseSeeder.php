b407b4a09b52317ac9f70bab5d42ab426fe5deba
Usitha Rupasinghe <usitha@arimaclanka.com>
2020-05-04 12:19:17 +0530

    database seeder file updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,19 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(TraditionalSongsTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
+        $this->call(SongCategoriesTableSeeder::class);
+        $this->call(TraditionalSongCategoriesTableSeeder::class);
+        $this->call(ProverbsTableSeeder::class);
+        $this->call(FrequentlyAskedQuestionTableSeeder::class);
+        $this->call(HandWorkCategoriesTableSeeder::class);
+        $this->call(HandWorksTableSeeder::class);
+        $this->call(HandWorkImagesTableSeeder::class);
+        $this->call(KnowledgeHubVideosTableSeeder::class);
+        $this->call(KnowledgeHubImagesTableSeeder::class);
+        $this->call(KnowledgeHubAudioTableSeeder::class);
+        $this->call(TraditionalGameTableSeeder::class);
+        $this->call(RiddleTableSeeder::class);
+        $this->call(NewCreationTableSeeder::class);
     }
 }
